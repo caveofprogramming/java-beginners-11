cabe6a0ac926dadce9a01da70a1750774eb37358
John Purcell <john.purcell@caveofprogramming.com>
2019-12-17 14:19:15 +0100

    Created

--- StreamIteration/src/application/App.java
+++ StreamIteration/src/application/App.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class App {
 
 	public static void main(String[] args) {
-		int[] numbers = {1, 3, 5, 7};
 		
-		Arrays.stream(numbers).forEach(i -> {
-			System.out.println(i);
+		String[] animals = {"dog", "bear", "horse"};
+		
+		Arrays.stream(animals).forEach(animal -> {
+			System.out.println(animal);
 		});
-
 	}
 
 }
--- StreamLoops/src/application/App.java
+++ StreamLoops/src/application/App.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.stream.IntStream;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IntStream.range(0, 5).forEach(i -> {
+			System.out.println(i);
+		});
 	}
 
 }
